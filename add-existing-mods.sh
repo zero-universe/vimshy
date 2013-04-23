1733e0f4fef23d831cd5f1d03ef85f6e7727ebf1
Oleg Lusan <zero.universe@gmail.com>
2013-04-23 18:19:25 +0200

    extended script: submodules will be cloned

--- add-existing-mods.sh
+++ add-existing-mods.sh
@@ -11,8 +11,15 @@ else
 	ln -s ~/.vim/vimrc ~/.vimrc
 fi
 
+# clone gits
+cd ~/.vim/
+git clone https://github.com/tpope/vim-fugitive.git bundle/fugitive
+git clone https://github.com/tpope/vim-unimpaired.git bundle/vim-unimpaired
+git clone https://github.com/vim-scripts/Pydiction.git bundle/pydiction 
+git clone https://github.com/godlygeek/tabular.git bundle/tabular
+git clone https://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
+
 echo "initialising and updating modules"
-cd ~/.vim
 git submodule update --init
 
 echo "all done ;-)"
